db92c4f38c86f9bbf5b6904c15fa06d5200bd3c6
Johnny Wilcox <anwi7210@colorado.edu>
2023-11-27 11:43:49 -0700

    Implemented modal functionality for the recipes array on the kitchen page

--- project-code/src/resources/js/javascript.js
+++ project-code/src/resources/js/javascript.js
@@ -1,6 +1,47 @@
-const RECIPE_MODAL = new bootstrap.Modal(document.getElementById('recipe-modal'));
+document.addEventListener('DOMContentLoaded', function () {
+    var modals = document.querySelectorAll('.modal');
 
-// Your existing JavaScript code for modal functionality (openRecipeModal, updateRecipeFromModal, updateDOM) goes here
+    modals.forEach(function (modal) {
+      new bootstrap.Modal(modal);
+    });
+  });
 
-// Initial rendering of recipes
-updateDOM();
+  function saveRecipeChanges(recipeId) {
+    // Get the values from the form
+    var recipeName = document.getElementById('recipeName').value;
+    var recipeText = document.getElementById('recipeText').value;
+  
+    const requestData = {
+      recipeName: recipeName,
+      recipeText: recipeText,
+    };
+
+    console.log(recipeName);
+    
+    fetch(`/kitchen/update/${recipeId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          
+          console.log('Recipe updated successfully:', data);
+        })
+        .catch(error => {
+          console.error('Error updating recipe:', error);
+        });
+    
+    var modalId = 'recipeModal' + recipeId;
+    var recipeModal = new bootstrap.Modal(document.getElementById(modalId));
+    recipeModal.hide();
+  }
+
+  
