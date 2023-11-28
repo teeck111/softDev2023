fab1de9f308ca30f04a72ac5fb14fbc1d6f18ee6
Aniket Chauhan <anch5121@colorado.edu>
2023-11-28 14:23:32 -0700

    get request3

--- project-code/src/index.js
+++ project-code/src/index.js
@@ -322,7 +322,8 @@ const all_user_ingredients =
   `SELECT DISTINCT *
   FROM users_to_ingredients u_to_i
   JOIN ingredients i ON u_to_i.ingredient_id=i.ingredient_id
-  WHERE u_to_i.user_id=$1;`;
+  WHERE u_to_i.user_id=$1
+  ORDER BY i.ingredient_text ASC;`;
 
 const all_unused_ingredients = 
   `SELECT *
@@ -332,13 +333,13 @@ const all_unused_ingredients =
     FROM users_to_ingredients u_to_i
     WHERE u_to_i.ingredient_id = i.ingredient_id
     AND u_to_i.user_id = $1
-  );`;
+  )
+  ORDER BY i.ingredient_text ASC;`;
 
 app.get('/pantry', async (req, res) => {
   var unused_ingredients = await db.any(all_unused_ingredients, [req.session.user.user_id]);
   db.any(all_user_ingredients, [req.session.user.user_id])
     .then((ingredients) => {
-      console.log(ingredients);
       res.render("pages/pantry.ejs", {
         ingredients,
         unused_ingredients,
@@ -373,14 +374,41 @@ app.post("/pantry/add", async (req, res) => {
                       VALUES
                         ($1, $2);`;
   var updated_ingredients = await db.none(add_query, [req.session.user.user_id, req.body.ingredient_id]);
-  
-  return res.redirect("/pantry"); });
-
-
-
-app.get('/favorites', (req, res) => {
-  res.render("pages/favorites.ejs",{session: req.session.user});
+  return res.redirect("/pantry");
 });
+
+app.post('/pantry/search', async (req, res) => {
+  var search_ingredients = 
+  `SELECT *
+  FROM ingredients i
+  WHERE NOT EXISTS (
+    SELECT 1
+    FROM users_to_ingredients u_to_i
+    WHERE u_to_i.ingredient_id = i.ingredient_id
+    AND u_to_i.user_id = $1
+  )
+  AND LOWER(i.ingredient_text) LIKE LOWER('${req.body.search_val}%')
+  ORDER BY i.ingredient_text ASC;`;
+  var unused_ingredients = await db.any(search_ingredients, [req.session.user.user_id]);
+  db.any(all_user_ingredients, [req.session.user.user_id])
+    .then((ingredients) => {
+      res.render("pages/pantry.ejs", {
+        ingredients,
+        unused_ingredients,
+        session: req.session.user
+      });
+    })
+    .catch((err) => {
+      res.render("pages/pantry.ejs", {
+        ingredients: [],
+        unused_ingredients: [],
+        error: true,
+        message: err.message,
+        session: req.session.user
+      });
+    });
+});
+
 
 app.get('/settings', async (req, res) => {
   const user_id = req.session.user.user_id;
@@ -407,10 +435,22 @@ app.get('/settings', async (req, res) => {
     console.error('Error fetching data:', err);
     res.status(400).send('Error fetching data');
   }
+
 });
 
 app.get("/favorites", (req, res) => {
-  res.render("pages/favorites.ejs",{session: req.session.user});
+  db.any("SELECT * FROM recipes WHERE user_id = $1 AND is_starred = 1", [req.session.user.user_id])
+    .then((recipes) => {
+      res.render('pages/favorites', { recipes, session: req.session.user, user_id: req.session.user.user_id,});
+      // Render the 'favorites' page with the 'recipes' array and 'user_id'
+      console.log('User ID:', req.session.user.user_id);
+
+    })
+
+    .catch((err) => {
+      res.render('pages/favorites', { recipes:[], session: req.session.user, user_id: req.session.user.user_id, display_recipe_index: req.query.recipe_index });
+      console.log(err);
+      });
 });
 
 
